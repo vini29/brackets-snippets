0bd93819c5df7531a329cd64e42ce8c5db4665ef
vini29 <vini29@gmail.com>
2016-03-02 19:38:52 -0300

    Translation for portuguese brazilian.

---
+++ nls/br/strings.js
@@ -0,0 +1,17 @@
+/*jshint maxlen:false */
+/*global define */
+
+define({
+    BUTTON_CANCEL:                      "Cancelar",
+    BUTTON_OK:                          "OK",
+    BUTTON_RESTORE_DEFAULTS:            "Restaurar padrao",
+    BUTTON_SAVE:                        "Salvar",
+    INSERT_SNIPPET:                     "Inserir snippet",
+    RESTART:                            "Reiniciar",
+    Q_RESTART_BRACKETS:                 "Deseja reiniciar o Brackets para aplicar novas configuracoes?",
+    SHORTCUTS:                          "Atalhos",
+    SHORTCUTS_HINT:                     "Atalhos - separar as teclas com scripts: Ctrl-Alt-Space",
+    SHOW_SNIPPET_PANEL:                 "Exibir painel snippets",
+    SNIPPETS_DIRECTORY:                 "Diretorio de snippets",
+    SNIPPETS_SETTINGS:                  "Configuracoes de snippets"
+});
